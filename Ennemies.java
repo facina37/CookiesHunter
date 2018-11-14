2d8837e6f3ea9960374bb2f11e2409a4e0b329a2
GabBru <33067666+GabBru@users.noreply.github.com>
2018-11-14 15:46:12 +0100

    Update Ennemies.java
    1st commit 1st step

--- Ennemies.java
+++ Ennemies.java
@@ -1,1 +1,30 @@
-fsb
+/**
+ * Décrivez votre classe Ennemies ici.
+ *
+ * @author Amandine Poullot @ Gabriel Brunet
+ * @version v0.1 2018-nov
+ */
+public class Ennemies extends Character
+{
+    private boolean isDead;
+    /**
+     * Constructeur d'objets de classe Ennemies
+     */
+    public Ennemies()
+    {
+    }
+    
+    /**
+     * Return the value of the boolean isDead.
+     */
+    public void getDead()
+    {
+    }
+    
+    /**
+     * Change the value of the boolean isDead;
+     */
+    public void setDead()
+    {
+    }
+}
